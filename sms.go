383861fc9963dc7135228cb8f04d7fe1f35fde74
jkarage <josephbkarage@gmail.com>
2023-12-28 11:21:36 +0300

    Updated response text

--- sms.go
+++ sms.go
@@ -8,13 +8,12 @@ import (
 )
 
 func SendSMS(service string) error {
-	message := fmt.Sprintf("Service %s is not currently in its active status, Current time: %v", service, time.Now())
+	message := fmt.Sprintf("Service %s is not currently in it's active state, Current time: %v", service, time.Now())
 	client := beemafrica.NewSMS()
-	resp, err := client.SendSMS(message, []string{"255713507067", "255615266021"}, "")
+	_, err := client.SendSMS(message, []string{"255713507067", "255615266021"}, "")
 	if err != nil {
 		return err
 	}
 
-	fmt.Println(resp)
 	return nil
 }
